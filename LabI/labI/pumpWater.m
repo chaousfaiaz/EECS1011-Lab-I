20b152598ccfe0a6fe2f64037df64a97f28ee0a2
chaousfaiaz <53284875+chaousfaiaz@users.noreply.github.com>
2025-04-09 10:23:31 -0400

    pumpWater.m

--- LabI/labI/pumpWater.m
+++ LabI/labI/pumpWater.m
@@ -19,4 +19,17 @@
 
 function pumpWater(a, litres)
     % write your code here!
+    % Measured calibration values 
+    flow_start_time = .55; %time for water to start flowing
+    time_per_litres = 23.6; % Time to pump 1 litre of water in s
+    % Calculate total pumping time
+    total_pump_time = flow_start_time+(litres*time_per_litres);
+    % Turn on pump 
+    writeDigitalPin(a, 'D2', 1);
+    pause(total_pump_time); % wait for the calculated time
+    % Turn off pump
+    writeDigitalPin(a, 'D2', 0);
+    % Display the pumping details
+    fprintf('pumped %.2f litres of water in %.2f seconds\n', litres, total_pump_time);
+    end
 end
