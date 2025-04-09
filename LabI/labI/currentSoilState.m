b355481362e369aee11d9a0356c10fb0be9e7a87
chaousfaiaz <53284875+chaousfaiaz@users.noreply.github.com>
2025-04-09 09:51:46 -0400

    Add files via upload

---
+++ LabI/labI/currentSoilState.m
@@ -0,0 +1,31 @@
+% function value = currentSoilState( x )
+%
+% Function   : currentSoilState
+%
+% Purpose    : 
+%
+% Parameters : 
+%
+% Return     :
+%
+% Calibration data:
+%  
+% Not immersed in water:    ___________ v
+%
+% Immersed in dry soil:     ___________ v
+%
+% Immersed in water-saturated soil:    ___________ v
+%
+% Immersed in water:    ___________ v
+%
+% Examples of Usage:
+%
+%    >> state = currentSoilState( a )
+% state =
+%
+%     "dry soil"
+%
+
+function state = currentSoilState( a )
+    % write your code here!
+end
---
+++ LabI/labI/pumpWater.m
@@ -0,0 +1,22 @@
+% function pumpWater( x )
+%
+% Function   : pumpWater
+%
+% Purpose    : 
+%
+% Parameters : 
+%
+% Calibration data:
+%  
+% Time to for water to begin flowing:    ___________ sec
+%
+% Time to empty 1 litre of water:     ___________ sec
+%
+% Examples of Usage:
+%
+%    >> pumpWater(a, litres)
+%
+
+function pumpWater(a, litres)
+    % write your code here!
+end
